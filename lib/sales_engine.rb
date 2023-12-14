22c484233dd7fc753cc5f744c2be63bc7478b7b1
Mel Langhoff <mlanghoff@uwalumni.com>
2023-12-14 15:07:40 -0700

    Add #from_csv and corresponding tests

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,4 +1,49 @@
 class SalesEngine
+    attr_reader :items, :merchants, :item_file_path, :merchant_file_path
 
+    def initialize
+        @items = []
+        @merchants = []
+    end
+
+    def self.from_csv(file_paths)
+        sales_engine = SalesEngine.new
+        sales_engine.load_items(file_paths[:items])
+        sales_engine.load_merchants(file_paths[:merchants])
+        sales_engine
+    end
+
+
+    def load_items(item_file_path)
+        CSV.foreach(item_file_path, headers: true) do |row|
+            id = row["id"].to_i
+            name = row["name"]
+            description = row["description"]
+            unit_price = row["unit_price"]
+            merchant_id = row["merchant_id"]
+            created_at = row["created_at"]
+            updated_at = row["updated_at"]
+            @items << Item.new(id: id.to_i, name: name, description: description, unit_price: unit_price, merchant_id: merchant_id, created_at: created_at, updated_at: updated_at)
+        end
+    end
+
+    def load_merchants(merchant_file_path)
+        CSV.foreach(merchant_file_path, headers: true) do |row|
+            id = row["id"].to_i
+            name = row["name"]
+            created_at = row["created_at"]
+            updated_at = row["updated_at"]
+            @merchants << Merchant.new(id: id.to_i, name: name, created_at: created_at, updated_at: updated_at)
+        end
+    end
 
 end
+
+# se = SalesEngine.from_csv({
+#   :items     => "./data/items.csv",
+#   :merchants => "./data/merchants.csv",
+# })
+# From there we can find the child instances:
+
+# items returns an instance of ItemRepository with all the item instances loaded
+# merchants returns an instance of MerchantRepository with all the merchant instances loaded
--- spec/sales_engine_spec.rb
+++ spec/sales_engine_spec.rb
@@ -2,9 +2,23 @@ require "./spec/spec_helper.rb"
 
 RSpec.describe SalesEngine do
     before :each do
-        sales_engine = SalesEngine.new
+        @sales_engine = SalesEngine.new
     end
 
     it "exists" do
-        expect(sales_engine).to be_an_instance_of SalesEngine
+        expect(@sales_engine).to be_an_instance_of SalesEngine
+    end
+
+    it "#from_csv" do
+        se = SalesEngine.from_csv({
+        :items     => "./data/items.csv",
+        :merchants => "./data/merchants.csv",
+        })
+        expect(se).to be_an_instance_of SalesEngine
+        expect(se.items).to be_a Array
+        expect(se.items.first).to be_an_instance_of Item
+        expect(se.merchants).to be_a Array
+        expect(se.merchants.first).to be_an_instance_of Merchant
+    end
+
 end
