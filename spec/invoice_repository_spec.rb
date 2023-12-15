d64d2b6cdf145ae0deb07f2fbfc885d30ec67ff8
Mel Langhoff <mlanghoff@uwalumni.com>
2023-12-14 18:54:18 -0700

    Add #all

--- spec/invoice_repository_spec.rb
+++ spec/invoice_repository_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe InvoiceRepository do
         loaded_invoices_from_csv_file = @invoicerepository
         expect(@invoicerepository.invoices.first.customer_id).to eq(1)
     end
+
+    it "#all" do
+        expect(@invoicerepository.all).to include Invoice
+    end 
 end
